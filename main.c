0f1ff57c7fcdcfc9a0a25261d1a1d8f171db9931
khy252 <khy25@sookmyung.ac.kr>
2025-11-02 21:10:55 +0900

    03

--- main.c
+++ main.c
@@ -4,24 +4,25 @@
 
 int main(int argc, char *argv[])
 {
-    int test_score[SIZE];
     int i;
-    int sum=0;
-    
-    printf("%i명 점수를 입력하세요\n", SIZE);  
+    int grade[SIZE];
+    int score[SIZE];
     
     for (i=0;i<SIZE;i++)
     {
-            scanf("%d", &test_score[i]);
-            sum += test_score[i];
+        grade[i] = rand()%100;
+    }
+     for (i=0;i<SIZE;i++)
+    {
+        score[i] = grade[i];
     }
     
-    #if 0
+    #if 1
     for (i=0;i<SIZE;i++)
-    printf("grade[%i] = %i\n", i, test_score[i]);
+        printf("grade[%i] = %i, grade[%i] = %i\n", i, grade[i], i, score[i]);
     #endif
-    printf("성적 평균 : %i\n", sum/SIZE);
     
   system("PAUSE");	
   return 0;
 }
+ 
