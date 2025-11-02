e98e33c99e9a9610e2fdb7b1efd2e6a7444c3f6b
khy252 <khy25@sookmyung.ac.kr>
2025-11-02 20:51:18 +0900

    02

--- main.c
+++ main.c
@@ -1,19 +1,27 @@
 
 
+#define SIZE        3
+
 int main(int argc, char *argv[])
 {
-    int test_score[5];
+    int test_score[SIZE];
     int i;
+    int sum=0;
     
-    test_score[0] = 10;
-    test_score[1] = 20;
-    test_score[2] = 30;
-    test_score[3] = 40;
-    test_score[4] = 50;
+    printf("%i명 점수를 입력하세요\n", SIZE);  
     
-    for (i=0;i<5;i++)
+    for (i=0;i<SIZE;i++)
+    {
+            scanf("%d", &test_score[i]);
+            sum += test_score[i];
+    }
+    
+    #if 0
+    for (i=0;i<SIZE;i++)
     printf("grade[%i] = %i\n", i, test_score[i]);
-  
+    #endif
+    printf("성적 평균 : %i\n", sum/SIZE);
+    
   system("PAUSE");	
   return 0;
 }
